fa91c8846f72d42ef092047fd77f4d0af7aa07d9
jamespeterjacob <jamespeterjacob@gmail.com>
2020-09-26 23:54:16 -0700

    load4

--- JIL/Startup.cs
+++ JIL/Startup.cs
@@ -15,6 +15,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using JIL.Areas.Identity;
 using JIL.Data;
+using System.Reflection;
 
 namespace JIL
 {
@@ -72,6 +73,41 @@ namespace JIL
                 endpoints.MapBlazorHub();
                 endpoints.MapFallbackToPage("/_Host");
             });
+        }
+
+        public class StartupProduction
+        {
+            public StartupProduction(IConfiguration configuration)
+            {
+                Configuration = configuration;
+                Console.WriteLine(MethodBase.GetCurrentMethod().DeclaringType.Name);
+            }
+
+            public IConfiguration Configuration { get; }
+
+            public void ConfigureServices(IServiceCollection services)
+            {
+                services.AddRazorPages();
+            }
+
+            public void Configure(IApplicationBuilder app)
+            {
+
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+
+                app.UseHttpsRedirection();
+                app.UseStaticFiles();
+
+                app.UseRouting();
+
+                app.UseAuthorization();
+
+                app.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapRazorPages();
+                });
+            }
         }
     }
 }
