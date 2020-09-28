16fa784d66a4a5c9ed5bc31600fecf9ac1c4983c
jamespeterjacob <jamespeterjacob@gmail.com>
2020-09-28 06:03:45 -0700

    load5

--- JIL/Startup.cs
+++ JIL/Startup.cs
@@ -75,39 +75,6 @@ namespace JIL
             });
         }
 
-        public class StartupProduction
-        {
-            public StartupProduction(IConfiguration configuration)
-            {
-                Configuration = configuration;
-                Console.WriteLine(MethodBase.GetCurrentMethod().DeclaringType.Name);
-            }
-
-            public IConfiguration Configuration { get; }
-
-            public void ConfigureServices(IServiceCollection services)
-            {
-                services.AddRazorPages();
-            }
-
-            public void Configure(IApplicationBuilder app)
-            {
-
-                app.UseExceptionHandler("/Error");
-                app.UseHsts();
-
-                app.UseHttpsRedirection();
-                app.UseStaticFiles();
-
-                app.UseRouting();
-
-                app.UseAuthorization();
-
-                app.UseEndpoints(endpoints =>
-                {
-                    endpoints.MapRazorPages();
-                });
-            }
-        }
+       
     }
 }
